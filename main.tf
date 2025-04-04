36873e3a7f3701680304b0f61f0818d81793abad
Satish Mahakalkar <99986280+Satishmahakalkar@users.noreply.github.com>
2025-04-04 17:09:32 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,46 +1,29 @@
 provider "aws" {
-  region = var.aws_region
+  region = "ap-south-1"
+}
+
+resource "aws_vpc" "main" {
+  cidr_block = "10.0.0.0/16"
+}
+
+resource "aws_subnet" "subnet1" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.0.1.0/24"
+  availability_zone = "ap-south-1a"
+}
+
+resource "aws_subnet" "subnet2" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.0.2.0/24"
+  availability_zone = "ap-south-1b"
+}
+
+resource "aws_internet_gateway" "gw" {
+  vpc_id = aws_vpc.main.id
 }
 
 resource "aws_ecs_cluster" "medusa_cluster" {
   name = "medusa-cluster"
 }
 
-# VPC, subnets, and ALB resources can be added here
-# For simplicity, let's assume you have default VPC (or you can create using Terraform too)
-
-resource "aws_ecs_task_definition" "medusa_task" {
-  family                   = "medusa-task"
-  network_mode             = "awsvpc"
-  requires_compatibilities = ["FARGATE"]
-  cpu                      = "512"
-  memory                   = "1024"
-  execution_role_arn       = var.execution_role_arn
-
-  container_definitions = jsonencode([
-    {
-      name      = "medusa"
-      image     = var.ecr_image_url
-      portMappings = [
-        {
-          containerPort = 9000
-          hostPort      = 9000
-        }
-      ]
-    }
-  ])
-}
-
-resource "aws_ecs_service" "medusa_service" {
-  name            = "medusa-service"
-  cluster         = aws_ecs_cluster.medusa_cluster.id
-  task_definition = aws_ecs_task_definition.medusa_task.arn
-  desired_count   = 1
-  launch_type     = "FARGATE"
-
-  network_configuration {
-    subnets         = var.subnets
-    security_groups = [var.security_group]
-    assign_public_ip = true
-  }
-}
+# More ECS service setup will be added later

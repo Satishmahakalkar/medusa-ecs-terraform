2f7681111b9ae14692ee0452c73e7843a1dab043
Satish Mahakalkar <99986280+Satishmahakalkar@users.noreply.github.com>
2025-04-04 17:10:05 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,11 +1,3 @@
-variable "aws_region" {
-  default = "us-east-1"
+variable "region" {
+  default = "ap-south-1"
 }
-
-variable "execution_role_arn" {}
-variable "ecr_image_url" {}
-variable "subnets" {
-  type = list(string)
-}
-
-variable "security_group" {}
